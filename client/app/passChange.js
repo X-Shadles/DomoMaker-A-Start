eebd004166cffa2f8868c204a2d51bb0474d1da6
X-Shadles <v2ty@hotmail.com>
2019-11-18 21:47:32 -0500

    passChange making

---
+++ client/app/passChange.js
@@ -0,0 +1,39 @@
+const handleSignup = (e) => {
+    e.preventDefault();
+
+    if ($('#user').val() == '' || $('#pass').val() == '' || $('#pass2').val() == '') {
+        handleError("all fields required");
+        return false;
+    }
+
+    if ($('#pass').val() !== $('#pass2').val()) {
+        handleError("passwords arn't the same");
+        return false;
+    }
+
+    sendAjax('POST', $('#signupForm').attr('action'), $('#signupForm').serialize(), redirect);
+
+    return false;
+}
+
+const PassWindow = (props) => {
+    return (
+        <form id="passForm" name="passForm"
+            onSubmit={handleLogin}
+            action="/changePass"
+            method="POST"
+            className="mainForm">
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <label htmlFor="pass2">Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password" />
+            <input className="formSubmit" type="submit" value="Change Pass" />
+        </form>
+    );
+};
+const createPassWindow = (csrf) => {
+    ReactDOM.render(
+        <PassWindow csrf={csrf} />,
+        document.querySelector('#content')
+    );
+};
