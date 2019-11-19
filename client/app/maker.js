e883ef5632f00f6a7c9ce0bfc6ac4487fb060d2c
X-Shadles <v2ty@hotmail.com>
2019-11-18 22:20:12 -0500

    moving pass-t

--- client/app/maker.js
+++ client/app/maker.js
@@ -65,6 +65,14 @@ const loadDomosFromServer = () => {
 };
 
 const setup = function(csrf) {
+    document.querySelector("#passChange").addEventListener("click", (e) => {
+        e.preventDefault();
+        ReactDOM.render(
+            <PassWindow csrf={csrf} />,
+        document.querySelector('#content')
+        );
+        return false;
+    });
     ReactDOM.render(
         <DomoForm csrf={csrf} />, document.querySelector('#makeDomo')
     );
@@ -85,3 +93,41 @@ const getToken = () => {
 $(document).ready(function() {
     getToken();
 });
+
+
+
+const changePassword = (e) => {
+    e.preventDefault();
+
+    if ($('#oldpass').val() == '' || $('#pass').val() == '' || $('#pass2').val() == '') {
+        handleError("all fields required");
+        return false;
+    }
+
+    if ($('#pass').val() !== $('#pass2').val()) {
+        handleError("passwords arn't the same");
+        return false;
+    }
+
+    sendAjax('POST', $('#passChange').attr('action'), $('#passChange').serialize(), redirect);
+
+    return false;
+};
+
+const PassWindow = (props) => {
+    return (
+        <form id="passForm" name="passForm"
+            onSubmit={changePassword}
+            action="/changePass"
+            method="POST"
+            className="mainForm">
+            <label htmlFor="pass">Old Password: </label>
+            <input id="oldpass" type="password" name="oldpass" placeholder="password" />
+            <label htmlFor="pass">New Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <label htmlFor="pass2">New Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password" />
+            <input className="newPass" type="submit" value="Change Pass" />
+        </form>
+    );
+};
