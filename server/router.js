19fb9ef8fa7df95b25be84e165fd748d6bf07b06
X-Shadles <v2ty@hotmail.com>
2019-12-09 13:54:57 -0500

    revert back

--- server/router.js
+++ server/router.js
@@ -4,7 +4,6 @@ const mid = require('./middleware');
 const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/getTwits', mid.requiresLogin, controllers.Twit.getTwits);
-  app.get('/PublicTwitter', mid.requiresLogin);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
