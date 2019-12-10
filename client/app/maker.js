ab6723fb9d829244928dedbf790f1f20b5c166ec
X-Shadles <v2ty@hotmail.com>
2019-12-09 20:41:58 -0500

    date change again

--- client/app/maker.js
+++ client/app/maker.js
@@ -42,13 +42,13 @@ const TwitList = function (props) {
     }
 
     const twitNodes = props.twits.map(function (twit) {
-        const fixedDate = Date();
+        const fixedDate = formatDate(Date());
         return (
             <div className="twit">
                 <div className="twitTweet">
                 <h3 className="twitName">{twit.username}:</h3>
                 <h3 className="twitText">{twit.tweet}</h3> 
-                <h3 className="twitTest">{fixedDate}</h3> 
+                <h3 className="twitTest">{twit.createdDate}</h3> 
                 </div>
             </div>
         );
