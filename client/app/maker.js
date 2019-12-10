76e16aea75ef9f92eec73b5909ffe84b6c5b6774
X-Shadles <v2ty@hotmail.com>
2019-12-09 20:56:05 -0500

    just until I say otherwise, its all gonna be date stuff

--- client/app/maker.js
+++ client/app/maker.js
@@ -42,7 +42,7 @@ const TwitList = function (props) {
     }
 
     const twitNodes = props.twits.map(function (twit) {
-        const fixedDate = formatDate(new Date());
+        const fixedDate = new Date();
         return (
             <div className="twit">
                 <div className="twitTweet">
